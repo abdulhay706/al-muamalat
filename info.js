646e04d42784bb0d5bfecda03692fb7e00a16437
abdulhay706 <tillayev706@gmail.com>
2026-05-19 10:40:35 +0500

    sign_in

---
+++ info.js
@@ -0,0 +1,47 @@
+console.log("Hello World");
+// ozgaruvchini elon qilish uchun kalit sozlar: var, let, const
+// console.log(a);
+// var a = 10;
+
+// let x=4
+// x=8
+// console.log(x);
+
+// const pi = 3.14;
+// pi=3.45
+// console.log(pi);
+// {
+//   {
+//     {
+//       {
+//         var a = 10;
+//       }
+//     }
+//   }
+//   let b=12
+//   console.log(b);
+  
+// }
+// let b=13
+// console.log(a);
+// console.log(b);
+
+//var global scope da elon qilinadi, let va const esa block scope da elon qilinadi.
+
+// var a=10
+// var a=20
+// console.log(a);
+
+// let a=12
+// let a=13
+// const b=13
+// const b=17
+// console.log(b);
+
+// ozgaruvchilarni elon qilishda quyidagi qoidalar amal qiladi:
+// 1. Ozgaruvchi nomi harf, $, _ bilan boshlanishi kerak.
+// 2. Ozgaruvchi nomida faqat harf, raqam, $, _ ishlatilishi mumkin.
+// 3. Ozgaruvchi nomida bo'sh joy bo'lmasligi kerak.
+// 4. Ozgaruvchi nomi kalit so'zlar bilan bir xil bo'lmasligi kerak (var, let, const, if, else, for, while va h.k.).
+// const firstName=''
+// const var=10
---
+++ script.js
@@ -0,0 +1,77 @@
+// console.log("Hello World");
+// const firstName=''
+// // const var=10
+// let a =15454
+// console.log(a)
+
+// const c=54
+// console.log(c)
+
+
+// dataTypelar
+// 1. Primitive-oddiy tur  -- Number, string, Boolean, null, undefined, Symbol, BigInt
+// 2.NanPrimitive ---object-{function,array}
+
+// Number
+// const a=12
+// const b=12.3
+// console.log(2+"2");
+// console.log(2-"2");
+// console.log(2/"2");
+// console.log(2*"2");
+// console.log(2*"dsd"); 
+
+//String
+// const name='Jasur'
+// const fam="asdasdasd"
+
+
+//Boolean--{true, false}
+const isActive=false
+const isAdd=true
+
+
+//  null
+let a=null
+a=12
+console.log(a);
+
+//  undefined
+let b
+console.log(b);
+
+
+
+// const sym=Symbol('asdasd')
+// console.log(sym);
+
+// BigInt
+// const a=132312423424234234234324324234324234234324
+
+
+// const ism=prompt('Ismingizni kiriting')
+// const age=Number(prompt('yoshingizni kiriting'))
+// console.log(ism,age);
+// // console.log(1+age);
+
+// alert(`Assalomu alaykum ${ism} siz ${2026-age}-yilda tugilgan ekansiz`) 
+// ENg kop ishlatiladigan string datatypening metodlari
+const ism=prompt('Avtomobillar turi')
+
+console.log(ism.trim().length)
+// console.log(ism.toUpperCase());
+// console.log(ism.toLowerCase());
+// console.log(ism.includes("A"));
+// console.log(ism.slice(0,4));
+// console.log(ism.startsWith('A'));
+// console.log(ism.endsWith('A'));
+// console.log(ism.indexOf('A'));
+// console.log(ism.charAt(0).toUpperCase()+ism.slice(1,ism.length).toLowerCase());
+
+
+
+
+
+
+
+
